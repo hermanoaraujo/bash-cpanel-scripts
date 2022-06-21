3362dc31f3ab0dd0b975417270d64a753562771a
Hermano Filho <39568925+hermanoaraujo@users.noreply.github.com>
2022-06-21 15:22:41 -0300

    Update fix-onapp-grub.sh

--- fix-onapp-grub.sh
+++ fix-onapp-grub.sh
@@ -2,7 +2,7 @@
 
 #BOOT_DISK=$(df -h | grep /boot | awk '{print $1}')
 
-mount /dev/vda /mnt && mount --bind /proc /mnt/proc/ && mount --bind /dev /mnt/dev&& mount --bind /sys /mnt/sys && chroot /mnt /bin/bash -l
+mount /dev/vda1 /mnt && mount --bind /proc /mnt/proc/ && mount --bind /dev /mnt/dev&& mount --bind /sys /mnt/sys && chroot /mnt /bin/bash -l
 sed -i 's/GRUB_ENABLE_BLSCFG.*/GRUB_ENABLE_BLSCFG=false/' /etc/default/grub
 grub2-mkconfig -o /boot/grub2/grub.cfg
 cd / && umount /mnt/proc && umount /mnt/dev && umount /mnt/sys && umount /mnt
